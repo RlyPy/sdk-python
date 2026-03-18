922b2b88daf960b54e9f6477ce7a70efde224903
RlyPy <support@rollypay.io>
2026-03-19 01:02:17 +0300

    Add __init__.py to resources package for correct distribution

---
+++ rollypay/resources/__init__.py
@@ -0,0 +1,13 @@
+from .terminals import Terminals
+from .payments import Payments
+from .stats import Stats
+from .misc import Misc
+from .base import Resource
+
+__all__ = [
+    "Terminals",
+    "Payments",
+    "Stats",
+    "Misc",
+    "Resource",
+]
